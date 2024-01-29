c87584e1e7758f6718f41c6654d1cbdfe0200447
Sarah French <sarah.french@hashicorp.com>
2024-01-29 14:00:45 +0000

    Make VCR_PATH an absolute path

--- .teamcity/components/builds/build_parameters.kt
+++ .teamcity/components/builds/build_parameters.kt
@@ -212,7 +212,7 @@ fun BuildType.enableProjectSweep(){
 // ParametrizedWithType.terraformEnableProjectSweeper unsets an environment variable used to skip the sweeper for project resources
 fun ParametrizedWithType.vcrEnvironmentVariables(config: AccTestConfiguration, providerName: String) {
     text("env.VCR_MODE", "RECORDING")
-    text("env.VCR_PATH", "./fixtures")
+    text("env.VCR_PATH", "%system.teamcity.build.checkoutDir%/fixtures")
     text("env.TEST", "./${providerName}/...")
     text("env.TESTARGS", "-run=%TEST_PREFIX%")
     hiddenVariable("env.GOOGLE_INFRA_PROJECT", config.infraProject, "The project that's linked to the GCS bucket storing VCR cassettes")
